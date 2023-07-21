1114d1e95cbb750c7d2cf1196d108e23e31daf99
Pasan Nethsara <pasannethsara@gmail.com>
2023-07-21 02:48:13 +0000

    routing done

--- testApp/src/app/heroes/heroes.component.ts
+++ testApp/src/app/heroes/heroes.component.ts
@@ -17,22 +17,21 @@ export class HeroesComponent {
   //   this.selectedHero = hero;
   // }
 
-  selectedHero?: Hero;
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  // }
 
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
